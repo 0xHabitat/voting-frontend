77d9fa4e1eda652dd0817237824efc925dc31232
Max Daunarovich <max.daunarovich@gmail.com>
2019-08-31 02:26:26 +0300

    Refactor VoteControls

--- src/volt/components/VoteControls/index.js
+++ src/volt/components/VoteControls/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import { Tx, Input, Output } from "leap-core";
+import Web3 from "web3";
+import { Tx, Input, Output, helpers } from "leap-core";
 import { providers, utils } from "ethers";
 import SMT from "../../lib/SparseMerkleTree";
 import { getStoredValue, storeValues } from "../../../services/localStorage";
+
+import votingBoothArtifact from "../../contracts/VotingBooth";
 
 import { Equation } from "./GridDisplay";
 import { Choice } from "./Choice";
@@ -14,10 +17,12 @@ import {
   ActionButton
 } from "./styles";
 import proposals from "../../proposals";
-import { votesToValue, getUTXOs } from "../../utils";
-import { abi, bytecode } from "../../contracts/voteBooth";
+import { votesToValue, getUTXOs, toHex, padHex, replaceAll } from "../../utils";
 import { voltConfig } from "../../config";
-import { getId } from "../../../services/plasma";
+import { getData, getId } from "../../../services/plasma";
+import Progress from "../Progress";
+import { bytecode, template } from "../../contracts/voteBooth";
+import Receipt from "../Receipt";
 
 const RPC = "https://testnet-node1.leapdao.org";
 const plasma = new providers.JsonRpcProvider(RPC);
@@ -28,14 +33,30 @@ class VoteControls extends Component {
     this.state = {
       expanded: false,
       votes: 0,
-      choice: ""
+      choice: "",
+      showProgress: false,
+      showReceipt: false
     };
     this.collapse = this.collapse.bind(this);
     this.expand = this.expand.bind(this);
+    this.setProgressState = this.setProgressState.bind(this);
+    this.setReceiptState = this.setReceiptState.bind(this);
+    this.resetState = this.resetState.bind(this);
+
     this.setTokenNumber = this.setTokenNumber.bind(this);
     this.setChoice = this.setChoice.bind(this);
+
+    this.prepareScript = this.prepareScript.bind(this);
+    this.getOutputs = this.getOutputs.bind(this);
+    this.constructVote = this.constructVote.bind(this);
+    this.signVote = this.signVote.bind(this);
+    this.processVote = this.processVote.bind(this);
+
     this.submit = this.submit.bind(this);
+
+    console.log(votingBoothArtifact.abi);
   }
+
   setTokenNumber(event) {
     const { target } = event;
     this.setState(state => ({
@@ -43,6 +64,7 @@ class VoteControls extends Component {
       votes: target.value
     }));
   }
+
   setChoice({ value }) {
     this.setState(state => ({
       ...state,
@@ -50,108 +72,275 @@ class VoteControls extends Component {
     }));
   }
 
-  async submit() {
-    const { account, metaAccount, web3 } = this.props;
-    console.log(account);
-    console.log({ metaAccount });
-    console.log({ web3 });
+  prepareScript() {
     const { proposalId } = this.props;
-    const { choice, votes } = this.state;
-    console.log(`Submit Choice: ${votes} for ${choice} from ${account}`);
-    const { hex, string } = votesToValue(votes);
-    const sign = choice === "Yes" ? 1 : -1;
-    const voiceCredits = sign * votes * 10 ** 18;
+    const { yesBoxAddress, noBoxAddress } = proposals[proposalId];
+    const hexId = toHex(proposalId, 12);
 
-    const { boothAddress } = proposals[0];
-    const balanceCardColor = voltConfig.BALANCE_CARD_COLOR;
-    const balanceCardAddress = voltConfig.CONTRACT_VOICE_BALANCE_CARD;
-    const balanceCards = await getUTXOs(plasma, account, balanceCardColor);
-    console.log({ balanceCards });
+    // Bytecode templates
+    const {
+      VOICE_CREDITS,
+      VOICE_TOKENS,
+      BALANCE_CARD,
+      YES_BOX,
+      NO_BOX,
+      PROPOSAL_ID
+    } = template;
+
+    // VOLT related addresses
+    const {
+      CONTRACT_VOICE_CREDITS,
+      CONTRACT_VOICE_TOKENS,
+      CONTRACT_VOICE_BALANCE_CARD
+    } = voltConfig;
+
+    // Replace all the placeholders with real data
+    let finalBytecode = replaceAll(
+      bytecode,
+      VOICE_CREDITS,
+      CONTRACT_VOICE_CREDITS
+    );
+    finalBytecode = replaceAll(
+      finalBytecode,
+      VOICE_TOKENS,
+      CONTRACT_VOICE_TOKENS
+    );
+    finalBytecode = replaceAll(
+      finalBytecode,
+      BALANCE_CARD,
+      CONTRACT_VOICE_BALANCE_CARD
+    );
+    finalBytecode = replaceAll(finalBytecode, YES_BOX, yesBoxAddress);
+    finalBytecode = replaceAll(finalBytecode, NO_BOX, noBoxAddress);
+
+    finalBytecode = replaceAll(finalBytecode, PROPOSAL_ID, hexId);
+    return Buffer.from(finalBytecode, "hex");
+  }
+
+  async getBalanceCard(address) {
+    // Balance Card
+    const { BALANCE_CARD_COLOR } = voltConfig;
+    const balanceCards = await getUTXOs(plasma, address, BALANCE_CARD_COLOR);
     const balanceCard = balanceCards[0];
-    const balanceCardId = getId(balanceCard);
 
-    const voiceCreditsColor = voltConfig.VOICE_CREDITS_COLOR;
+    return {
+      id: getId(balanceCard),
+      unspent: balanceCard
+    };
+  }
+
+  async getVoteCredits(address) {
+    const { VOICE_CREDITS_COLOR } = voltConfig;
     const voiceCreditsUTXOs = await getUTXOs(
       plasma,
-      account,
-      voiceCreditsColor
+      address,
+      VOICE_CREDITS_COLOR
     );
-    console.log({ voiceCreditsUTXOs });
 
+    // TODO: We can put all UTXO here for consolidation
+    return {
+      unspent: voiceCreditsUTXOs[0]
+    };
+  }
+
+  async getGas(address) {
+    // Gas is always paid in Leap tokens (color = 0)
+    const LEAP_COLOR = 0;
+    const gasUTXOs = await getUTXOs(plasma, address, LEAP_COLOR);
     // TODO: Do we need to get several here?
-    const gasUTXOs = await getUTXOs(plasma, boothAddress, 0);
-    console.log(gasUTXOs);
     const gas = gasUTXOs[0];
-    const script = Buffer.from(bytecode, "hex");
+    return {
+      unspent: gas
+    };
+  }
 
-    const condition = Tx.spendCond(
-      [
-        new Input({
-          prevout: gas.outpoint,
-          script
-        }),
-        new Input({
-          prevout: balanceCard.outpoint
-        }),
-        new Input({
-          prevout: voiceCreditsUTXOs[1].outpoint // HARDCODED, FIX TO PROPER SOLUTION!
-        })
-      ],
-      [
-        // Empty for now, we can always get them back from check
-      ]
+  async getVoteTokens(address) {
+    const { VOICE_TOKENS_COLOR } = voltConfig;
+    const voiceTokensUTXOs = await getUTXOs(
+      plasma,
+      address,
+      VOICE_TOKENS_COLOR
     );
+
+    // TODO: Pick enough outputs
+    const voiceTokensOutput = voiceTokensUTXOs[0];
+
+    return {
+      unspent: voiceTokensOutput
+    };
+  }
+
+  async getOutputs() {
+    const { account, proposalId } = this.props;
+    const proposal = proposals[proposalId];
+    const { boothAddress } = proposal;
+
+    // TODO: Parallelize with Promise.all([...promises])
+    const gas = await this.getGas(boothAddress);
+    const voteTokens = await this.getVoteTokens(boothAddress);
+    const balanceCard = await this.getBalanceCard(account);
+    const voteCredits = await this.getVoteCredits(account);
+
+    return {
+      gas,
+      voteTokens,
+      balanceCard,
+      voteCredits
+    };
+  }
+
+  cookVoteParams(balanceCardId) {
+    const { account, proposalId } = this.props;
+    const { votes, choice } = this.state;
+
+    // Implement NO vote with negative value
+    const sign = choice === "Yes" ? 1 : -1;
 
     let tree;
     let castedVotes;
+
     let localTree = getStoredValue("votes", account);
+
     if (!localTree) {
       console.log("local tree is empty");
       tree = new SMT(9);
-      castedVotes = 0;
+      castedVotes = padHex("0x0", 64);
     } else {
+      console.log({ localTree });
       const parsedTree = JSON.parse(localTree);
       console.log({ parsedTree });
       tree = new SMT(9, parsedTree);
       castedVotes = parsedTree[proposalId];
+      console.log("Tree root:", tree.root);
     }
-    const proof = tree.createMerkleProof(proposalId);
-    window.tree = tree;
+    const proof = tree.createMerkleProof(0);
 
-    console.log({balanceCardId, proof, votes, castedVotes});
+    const prevNumOfVotes = utils.parseEther("1".toString()); // get this one from tree
+    const newNumOfVotes = utils.parseEther("2".toString()); // this prevNum and new votes
 
-    const voteBoothABI = new utils.Interface(abi);
-    const castBallotParams = voteBoothABI.functions.castBallot.encode([
+    const { abi } = votingBoothArtifact;
+    const contractInterface = new utils.Interface(abi);
+
+    return contractInterface.functions.castBallot.encode([
       parseInt(balanceCardId),
       proof,
-      parseInt(votes),
-      castedVotes
+      prevNumOfVotes, // previous value
+      newNumOfVotes // how much added
     ]);
-    console.log(castBallotParams);
+  }
 
-    condition.inputs[0].setMsgData(castBallotParams);
+  async constructVote(outputs, script, data) {
+    const { gas, voteTokens, balanceCard, voteCredits } = outputs;
 
-    // Sign Spending Condition
-    if (metaAccount && metaAccount.privateKey) {
-      // TODO: Check that this is working on mobile, where you don't have Metamask
-      condition.signAll(metaAccount.privateKey);
-    } else {
-      console.log("Waiting for sign with metamask");
-      await condition.signWeb3(web3);
-      console.log("Signed!");
-    }
-
-    // Check Spending Condition
-    const response = await plasma.send("checkSpendingCondition",
-      [condition.hex()]
+    const vote = Tx.spendCond(
+      [
+        new Input({
+          prevout: gas.unspent.outpoint,
+          script
+        }),
+        new Input({
+          prevout: voteTokens.unspent.outpoint
+        }),
+        new Input({
+          prevout: balanceCard.unspent.outpoint
+        }),
+        new Input({
+          prevout: voteCredits.unspent.outpoint
+        })
+      ],
+      // Outputs is empty, cause it's hard to guess what it should be
+      []
     );
 
-    console.log("Spending condition checked");
-    console.log({ response });
+    vote.inputs[0].setMsgData(data);
 
-    // TODO: Update outputs accordingly
-    // TODO: Submit transition to blockchain
+    return vote;
+  }
+
+  async signVote(vote) {
+    const { metaAccount, web3 } = this.props;
+    console.log(web3);
+    if (metaAccount && metaAccount.privateKey) {
+      // TODO: Check that this is working on mobile, where you don't have Metamask
+      vote.signAll(metaAccount.privateKey);
+    } else {
+      await window.ethereum.enable();
+      await vote.signWeb3(web3);
+    }
+  }
+
+  async checkVote(vote) {
+    return await plasma.send("checkSpendingCondition", [vote.hex()]);
+  }
+
+  async updateVoteOutputs(vote, correctOutputs) {
+    for (let i = 0; i < correctOutputs.length; i++) {
+      vote.outputs[i] = new Output.fromJSON(correctOutputs[i]);
+    }
+  }
+
+  async processVote(vote) {
+    const plasmaWeb3 = helpers.extendWeb3(new Web3(RPC));
+    const receipt = await plasmaWeb3.eth.sendSignedTransaction(vote.hex());
+    return receipt;
+  }
+
+  async submit() {
+    console.log("Display Progress Screen");
+    this.setProgressState(true);
+
+    /// START NEW CODE
+
+    const script = this.prepareScript();
+    const outputs = await this.getOutputs();
+    const { balanceCard } = outputs;
+    const data = this.cookVoteParams(balanceCard.id);
+    const vote = await this.constructVote(outputs, script, data);
+
+    console.log({ vote });
+
+    // Sign and check vote
+    await this.signVote(vote);
+    const check = await this.checkVote(vote);
+
+    // Update vote and sign again
+    this.updateVoteOutputs(vote, check.outputs);
+    await this.signVote(vote);
+    const secondCheck = await this.checkVote(vote);
+    console.log({ secondCheck });
+
+    this.setProgressState(false);
+    this.setReceiptState(true);
+
+    // Submit vote to blockchain
+    // const receipt = await this.processVote(vote);
+
     // TODO: Update local SMT
+    // TODO: Show receipt
+  }
+
+  setProgressState(bool) {
+    this.setState(state => ({
+      ...state,
+      showProgress: bool
+    }));
+  }
+
+  setReceiptState(bool) {
+    this.setState(state => ({
+      ...state,
+      showReceipt: bool
+    }));
+  }
+
+  resetState() {
+    this.setState(() => ({
+      expanded: false,
+      votes: 0,
+      choice: "",
+      showProgress: false,
+      showReceipt: false
+    }));
   }
 
   collapse() {
@@ -162,6 +351,7 @@ class VoteControls extends Component {
       };
     });
   }
+
   expand() {
     this.setState(state => {
       return {
@@ -170,17 +360,24 @@ class VoteControls extends Component {
       };
     });
   }
+
   render() {
     const { expanded, votes, choice } = this.state;
+    const { showReceipt, showProgress } = this.state;
     const { credits } = this.props;
     const max = Math.floor(Math.sqrt(credits)) || 0;
     const options = [
-      { value: "Yes", color: "voltBrandGreen" },
-      { value: "No", color: "voltBrandRed" }
+      { value: "yes", color: "voltBrandGreen" },
+      { value: "no", color: "voltBrandRed" }
     ];
     const disabled = votes < 1 || choice === "";
+    console.log(this.state);
     return (
       <Container>
+        {showProgress && <Progress />}
+        {showReceipt && (
+          <Receipt voteType={choice} votes={votes} onClose={this.resetState} />
+        )}
         <Equation votes={votes} />
         <StyledSlider
           min={0}
