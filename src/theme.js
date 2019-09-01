9a5caf05ba5f87c22115ad82b396618ff68c4199
Max Daunarovich <max.daunarovich@gmail.com>
2019-09-02 00:03:43 +0300

    Add grey colors to list

--- src/theme.js
+++ src/theme.js
@@ -41,12 +41,15 @@ export default {
   },
   colors: {
     voltBrandMain: '#582C83',
-    voltBrandWhite: '#FFFFFF',
     voltBrandGreen: '#41CC8F',
     voltBrandGreenBorder: '#0ceba1',
     voltBrandRed: '#FF2C14',
     voltBrandRedBorder: '#ff6d58',
     voltBrandDimPurple: '#8a6ca8',
+    voltBrandWhite: '#FFFFFF',
+    voltBrandLightGrey: '#F0F0F0',
+    voltBrandGrey: '#D9E1E2',
+    voltBrandDarkGrey: '#383838',
 
     passportUSA: "#6846c3",
     passportUSB: "#077154",
