8f7d1a0516a296e872b2c91b2ba59c638dd39dd5
Max Daunarovich <max.daunarovich@gmail.com>
2019-09-02 00:04:42 +0300

    Create proposal list parent and child components

---
+++ src/volt/components/ProposalsList/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Flex } from "rimble-ui";
+
+export const ListContainer = styled(Flex).attrs(() => ({}))`
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.voltBrandWhite};
+  flex: 1;
+`;
