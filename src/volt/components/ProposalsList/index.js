ce8689f558dea39f128c977621382e9da24745a3
Max Daunarovich <max.daunarovich@gmail.com>
2019-09-03 13:45:47 +0300

    Add array index to key to clear console warnings

--- src/volt/components/ProposalsList/index.js
+++ src/volt/components/ProposalsList/index.js
@@ -1,19 +1,18 @@
-import React from "react";
+import React, { memo } from "react";
 import { ListContainer } from "./styles";
 import SingleProposal from "./SingleProposal";
 
 const ProposalsList = props => {
   const { list, toggle, favorites } = props;
-
   return (
     <ListContainer>
       {list &&
-        list.map(proposal => {
+        list.map((proposal, index) => {
           const { proposalId } = proposal;
           const favorite = favorites[proposalId];
           return (
             <SingleProposal
-              key={proposalId}
+              key={`${proposalId}${index}`}
               toggle={toggle}
               favorite={favorite}
               {...proposal}
@@ -24,4 +23,4 @@ const ProposalsList = props => {
   );
 };
 
-export default ProposalsList;
+export default ProposalsList
