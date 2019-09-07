40b39b7a2d399da7e1d636dc7818f191e5082eb2
Johann Barbie <johannbarbie@me.com>
2019-09-07 23:44:54 +0200

    only show results after End Time

--- src/volt/components/Menu/index.js
+++ src/volt/components/Menu/index.js
@@ -1,19 +1,20 @@
 import React from "react";
+import dayjs from 'dayjs';
 import { FullScreenContainer, ActionClose } from "../Common";
 import { Container } from "../Progress/styles";
 import { DeoraLogo, MenuItems, Item, AccountDetails, Label, Address } from "./styles";
 
-const Menu = ({ onClose, account }) => (
+const Menu = ({ onClose, account, voteEndTime }) => (
   <FullScreenContainer>
     <Container>
       <DeoraLogo />
       <MenuItems>
-        <Item target="_blank" href="https://www.deora.earth/volt-platform/about">Über uns</Item>
-        <Item target="_blank" href="https://www.deora.earth/volt-platform/quadratic">Quadratical Voting</Item>
-        <Item target="_blank" href="https://www.deora.earth/volt-platform/blockchain">Die Blockchain</Item>
-        <Item target="_blank" href="https://www.deora.earth/volt-platform/privacy">Sicherheit & Privacy</Item>
-        <Item target="_blank" href="https://www.deora.earth/volt-platform/extra-proposals">Weitere Anträge</Item>
-        <Item target="_blank" href="https://volt.deora.earth/results">Wahlergebnisse</Item>
+        <Item href="https://www.deora.earth/volt-platform/about">Über uns</Item>
+        <Item href="https://www.deora.earth/volt-platform/quadratic">Quadratical Voting</Item>
+        <Item href="https://www.deora.earth/volt-platform/blockchain">Die Blockchain</Item>
+        <Item href="https://www.deora.earth/volt-platform/privacy">Sicherheit & Privacy</Item>
+        <Item href="https://www.deora.earth/volt-platform/extra-proposals">Weitere Anträge</Item>
+        {dayjs().isAfter(voteEndTime) && <Item target="_blank" href="https://volt.deora.earth/results">Wahlergebnisse</Item>}
       </MenuItems>
       <AccountDetails>
         <Label>Your Account Address:</Label>
